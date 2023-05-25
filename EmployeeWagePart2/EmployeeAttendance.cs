290d8ca988fde65f1db3af2fc4de2f49c2f94817
Ganapati <ganapatasakatwa@gmail.com>
2023-05-25 06:21:32 +0530

    [GK] Checking Employee is Present or Absent

---
+++ EmployeeWagePart2/EmployeeAttendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagePart2
+{
+    public class EmployeeAttendance
+    {
+        public void EmpAttendance()
+        {
+            //UC1--Employee Present or Absent
+            int fulltime = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if(empcheck==fulltime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWagePart2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EmployeeWagePart2
+{
+    public class Pragram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem Part 2");
+            Console.WriteLine("Check whether employee is Present or Absent");
+            //Creating Object of the class to call in Main method
+            //because method is non static
+            EmployeeAttendance employeeAttendance=new EmployeeAttendance();
+            employeeAttendance.EmpAttendance();
+        }
+    } 
+}
