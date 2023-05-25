94a1a8838d9544b86132cc3c7e20f32f414acca0
Ganapati <ganapatasakatwa@gmail.com>
2023-05-25 06:44:35 +0530

    [GK] Employee Daily Wage Calculation

---
+++ EmployeeWagePart2/EmployeeWage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagePart2
+{
+    public class EmployeeWage
+    {
+        public void EmpWage()
+        {
+            //defined Constants
+            int fulltime = 1;
+            int waheperhr = 20;
+
+            //variables
+            int empwage = 0;
+            int emphrs = 0;
+
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+
+            if (empcheck == fulltime)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empwage = emphrs * waheperhr;
+            Console.WriteLine("Employee Wage is--"+empwage);
+        }
+
+       
+    }
+}
--- EmployeeWagePart2/Program.cs
+++ EmployeeWagePart2/Program.cs
@@ -6,11 +6,27 @@ namespace EmployeeWagePart2
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem Part 2");
-            Console.WriteLine("Check whether employee is Present or Absent");
-            //Creating Object of the class to call in Main method
-            //because method is non static
-            EmployeeAttendance employeeAttendance=new EmployeeAttendance();
-            employeeAttendance.EmpAttendance();
+            while(true)
+            {
+                Console.WriteLine("Select the choice\n1)Employee Present or Absent\n2)Employee Daily Wage");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        //Creating Object of the class to call in Main method
+                        //because method is non static
+                        EmployeeAttendance employeeAttendance = new EmployeeAttendance();
+                        employeeAttendance.EmpAttendance();
+                        break;
+
+                    case 2:
+                        EmployeeWage employeeWage=new EmployeeWage();
+                        employeeWage.EmpWage();
+                        break;
+                }
+            }
+           
+            
         }
     } 
 }
